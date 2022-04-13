5aeba5e8167d6772161529b70da11701042994b0
Matheus Vasconcelos <matheus.vasco.roch@gmail.com>
2022-04-13 12:00:57 -0300

    change title home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,8 @@ import "./Home.css";
 export default () => 
     <section id="home" className="home">
         <div>
-            <h1>Welcome</h1>
-            <h1 id="typeWriter">I'm </h1>
+            <h1>Matheus Vasconcelos</h1>
+            <h1 id="typeWriter">Front End Developer</h1>
         </div>
         <a href="#contact"><button>Contato</button></a>
     </section>
